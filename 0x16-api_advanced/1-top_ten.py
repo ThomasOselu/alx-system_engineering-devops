cc1b0c770877d351cb497d89d19257827dea60a8
ThomasOselu <thomasoselu1@gmail.com>
2023-08-08 20:30:16 +0000

    changes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
-prints the titles of the first 10 hot posts
-listed for a given subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed"""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
